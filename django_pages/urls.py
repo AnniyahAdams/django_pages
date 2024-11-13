11b772dace882ea3161eea800950f8f6103cabd7
AnniyahAdams <aada187@lsu.edu>
2024-11-12 23:41:56 -0600

    Update urls.py

--- django_pages/urls.py
+++ django_pages/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
-from django.contrib import admin
 from django.urls import path
+from .views import HomePageView, AboutPageView
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
+    path('about/', AboutPageView.as_view(), name='about'),
+    path('', HomePageView.as_view(), name='home'),
 ]
